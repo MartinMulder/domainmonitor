2f519d6cafbc7e9d1d1cee62600152db211da373
Martin Mulder <m.j.mulder@provinciegroningen.nl>
2019-09-20 09:49:11 +0200

    Changed default order of auditlogging to DESC

--- app/Models/AuditLog.php
+++ app/Models/AuditLog.php
@@ -2,11 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class AuditLog extends Model
 {
     public $fillable = ['action', 'result', 'user', 'description'];
+
+    protected static function boot()
+    {
+    	parent::boot();
+
+    	static::addGLobalScope('order', function(Builder $builder) {
+    		$builder->orderBy('id', 'desc');
+    	});
+    }
 
     public function auditable()
     {
